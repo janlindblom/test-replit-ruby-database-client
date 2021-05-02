8b13fd74d2daf92982d03d4b22377e9ebf4c7b72
Jan Lindblom <janlindblom@fastmail.fm>
2021-05-02 09:42:51 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'replit'
+
+set :protection, :except => :frame_options
+set :bind, '0.0.0.0'
+set :port, 8080
+
+@db = Replit::Database::Client.new
+@db.set('visitors', 0)
+
+get '/' do
+  @db = Replit::Database::Client.new
+  visitors = @db.get('visitors')
+  visitors += 1
+  @db.set('visitors', visitors)
+  "Hello visitor #{visitors}!"
+end
